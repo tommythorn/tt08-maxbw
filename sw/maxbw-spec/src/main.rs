d97c4faebca074860bb2ebffd41e441361ebb699
Tommy Thorn <tommy-github2@thorn.ws>
2024-07-24 18:07:17 -0700

    client side using pending arrays

--- sw/maxbw-spec/src/main.rs
+++ sw/maxbw-spec/src/main.rs
@@ -10,6 +10,8 @@ use rand::Rng;
 use std::sync::mpsc::{self, Receiver, Sender};
 use std::thread;
 
+const WINDOW_SIZE: usize = 16; // XXX To nail down *exactly*
+
 struct Endpoint<S: std::fmt::Debug, R: std::fmt::Debug> {
     tx: Sender<S>,
     rx: Receiver<R>,
@@ -61,7 +63,9 @@ fn main() {
 }
 
 fn client(ep: Endpoint<Command, Reply>) {
-    let mut pending_loads = std::collections::BTreeMap::new();
+    let mut pending_reads = vec![None; WINDOW_SIZE];
+    let mut pending_reads_count = 0;
+    let mut oldest_read_tag = 0;
     let mut read_tag = 0;
     let mut data_tag = 0;
     let mut paused = false;
@@ -77,21 +81,22 @@ fn client(ep: Endpoint<Command, Reply>) {
 
     let mut n = 1;
     loop {
-        if !paused && n < 100 {
+        if !paused && n < 100 && read_tag - oldest_read_tag < WINDOW_SIZE {
+            let is_read = rand::thread_rng().gen_range(0..100) < 66;
             let magic_number = rand::thread_rng().gen_range(1..6);
-            let is_read = /*magic_number % 2 == 0*/ true;
             let length = 1 << (magic_number / 2);
             let a = rand::thread_rng().gen_range(1..1000u64);
             if is_read {
-                pending_loads.insert(read_tag, a);
+                assert!(pending_reads[read_tag % WINDOW_SIZE].is_none());
+                pending_reads[read_tag % WINDOW_SIZE] = Some(a);
                 read_tag += 1;
+                pending_reads_count += 1;
                 ep.send(Command::Read(length, a));
-                //println!("client: pending loads: {pending_loads:?}");
                 n += 1;
             } else {
                 ep.send(Command::Write(a, vec![1u8; length.into()]));
             }
-        } else if pending_loads.is_empty() {
+        } else if pending_reads_count == 0 {
             ep.send(Command::EndSim);
             return;
         } else {
@@ -102,7 +107,9 @@ fn client(ep: Endpoint<Command, Reply>) {
         match ep.receive() {
             Reply::Idle => {}
             Reply::Synced => {
-                pending_loads.clear();
+                pending_reads = vec![None; WINDOW_SIZE];
+                pending_reads_count = 0;
+                oldest_read_tag = 0;
                 read_tag = 0;
                 data_tag = 0;
                 paused = false;
@@ -111,15 +118,24 @@ fn client(ep: Endpoint<Command, Reply>) {
             Reply::Resume => paused = false,
             Reply::Data(delta, data) => {
                 let this = data_tag + delta as i32;
-                match pending_loads.remove(&this) {
+                match pending_reads[this as usize % WINDOW_SIZE] {
                     None => panic!(
-                        "client: got data for unknown load #{this} \
+                        "client: got data for unknown read #{this} \
 			 (delta {delta}, data_tag {data_tag}"
                     ),
-                    Some(a) => println!(
-                        "client: load #{this} {data:?} address {a} (delta {})",
-                        this - data_tag
-                    ),
+                    Some(a) => {
+                        println!(
+                            "client: read #{this} {data:?} address {a} (delta {})",
+                            this - data_tag
+                        );
+                        pending_reads[this as usize % WINDOW_SIZE] = None;
+                        pending_reads_count -= 1;
+                        while oldest_read_tag < read_tag
+                            && pending_reads[oldest_read_tag % WINDOW_SIZE].is_none()
+                        {
+                            oldest_read_tag += 1;
+                        }
+                    }
                 }
                 data_tag += 1;
             }
@@ -130,7 +146,7 @@ fn client(ep: Endpoint<Command, Reply>) {
 fn memory_server(ep: Endpoint<Reply, Command>) {
     let mut tag = 0isize;
     let mut data_tag = 0isize;
-    let mut pending_loads = std::collections::BTreeMap::new();
+    let mut pending_reads = std::collections::BTreeMap::new();
 
     // Sending idle isn't required but reflects what would happen on hardware
     ep.send(Reply::Idle);
@@ -142,12 +158,12 @@ fn memory_server(ep: Endpoint<Reply, Command>) {
             Command::Sync => {
                 tag = 0;
                 data_tag = 0;
-                pending_loads.clear();
+                pending_reads.clear();
                 ep.send(Reply::Synced);
             }
             Command::Write(_a, _d) => {}
             Command::Read(w, a) => {
-                pending_loads.insert(tag, (w, a));
+                pending_reads.insert(tag, (w, a));
                 tag += 1;
             }
             Command::EndSim => return,
@@ -163,22 +179,22 @@ fn memory_server(ep: Endpoint<Reply, Command>) {
             pause_requested = !pause_requested;
         }
 
-        if !pending_loads.is_empty() && rand::thread_rng().gen_range(0..100) < 20 {
+        if !pending_reads.is_empty() && rand::thread_rng().gen_range(0..100) < 20 {
             /*println!(
-                "\t\tserver: {} pending loads: {:?}",
-                pending_loads.len(),
-                pending_loads
+                "\t\tserver: {} pending reads: {:?}",
+                pending_reads.len(),
+                pending_reads
             );*/
-            let i = rand::thread_rng().gen_range(0..pending_loads.len());
-            let target_load_tag: isize = *(pending_loads.keys().nth(i).unwrap());
-            let (tag2, (w, a)) = pending_loads.remove_entry(&target_load_tag).unwrap();
-            assert_eq!(target_load_tag, tag2);
+            let i = rand::thread_rng().gen_range(0..pending_reads.len());
+            let target_read_tag: isize = *(pending_reads.keys().nth(i).unwrap());
+            let (tag2, (w, a)) = pending_reads.remove_entry(&target_read_tag).unwrap();
+            assert_eq!(target_read_tag, tag2);
             println!(
-                "\t\tserver: processing load #{target_load_tag} {w}B at {a} \
+                "\t\tserver: processing read #{target_read_tag} {w}B at {a} \
 		 (delta {} data_tag {data_tag})",
-                target_load_tag - data_tag
+                target_read_tag - data_tag
             );
-            let delta = target_load_tag - data_tag;
+            let delta = target_read_tag - data_tag;
             assert_eq!(delta, isize::from(delta as i8)); // XXX Narrow that range
             ep.send(Reply::Data(delta as i8, vec![0u8; w as usize]));
             data_tag += 1;
